bc7010a7a80be26f550be1998b1aa5d2813492c7
Yan Hankin <hankinyb@gmail.com>
2018-01-17 23:05:28 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element)  
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array = array.shift()
+}
+
+function removeElementFromBeginningofArray(array) {
+  return array.slice(-2)
+  }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop()
+}
+
+function removeELementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
