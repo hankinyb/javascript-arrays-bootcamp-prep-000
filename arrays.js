17b48dcf4162cb0d59928f3416a1c46e319cb1e1
Yan Hankin <hankinyb@gmail.com>
2018-01-17 23:31:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return [element, ...array]
+  return array.unshift(element)
 }
 
 function addElementToEndOfArray(array, element) {
