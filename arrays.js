2bbc8730cce56d462dc09ec13929ca0dd381954d
Yan Hankin <hankinyb@gmail.com>
2018-01-17 23:41:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array = array.unshift(element)
+  array.unshift(element)
   return array 
 }
 
@@ -14,7 +14,7 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array = array.push(element) 
+  array.push(element) 
   return array
 }
 
